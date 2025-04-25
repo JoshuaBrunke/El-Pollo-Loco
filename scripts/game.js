3acd62be23faacb41dbc5a9d086c93062ed032e2
Joshua Brunke <seafortghost@gmail.com>
2025-04-25 17:33:40 +0200

    Cleaned up code, added canvas width and height to loadCanvas

--- scripts/game.js
+++ scripts/game.js
@@ -9,6 +9,8 @@ function init() {
 
 function loadCanvas() {
     canvas = document.getElementById("canvas");
+    canvas.width = 720;
+    canvas.height = 480;
     ctx = canvas.getContext("2d");
     character.src = "./assets/img/2_character_pepe/2_walk/W-21.png";
     character.onload = function() {
