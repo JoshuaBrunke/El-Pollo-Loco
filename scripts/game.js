9ec47ad45cb33d75b7a8d187cb17c9391560aaac
Joshua Brunke <seafortghost@gmail.com>
2025-04-25 20:35:53 +0200

    folder restructuring

--- scripts/game.js
+++ scripts/game.js
@@ -14,7 +14,7 @@ function loadCanvas() {
     ctx = canvas.getContext("2d");
     character.src = "./assets/img/2_character_pepe/2_walk/W-21.png";
     character.onload = function() {
-        ctx.drawImage(character, 20, 10, 50, 150);
+        ctx.drawImage(character, 20, 300, 50, 150);
     }
 
 
