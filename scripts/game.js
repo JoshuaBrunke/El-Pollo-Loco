da2927b79f11ab3d3e57951a35229b4eb789e3af
Joshua Brunke <seafortghost@gmail.com>
2025-04-25 17:28:07 +0200

    Added loadCanvas function, placed charactger on canvas

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,19 @@
+let canvas;
+let ctx;
+let character = new Image();
+
 function init() {
     addAttributesToExternalLinks();
+    loadCanvas();
+}
+
+function loadCanvas() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    character.src = "./assets/img/2_character_pepe/2_walk/W-21.png";
+    character.onload = function() {
+        ctx.drawImage(character, 20, 10, 50, 150);
+    }
+
+
 }
