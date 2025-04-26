9b2ff147003480a71e90b7ca461f2d78aaf6d1f7
Joshua Brunke <seafortghost@gmail.com>
2025-04-26 18:21:16 +0200

    Added character and chicken classes

---
+++ models/character.class.js
@@ -0,0 +1,30 @@
+class Character {
+x;
+y;
+
+moveRight () {
+
+}
+
+jump() {
+
+}
+
+
+    /*
+    constructor(name, level, health) {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+    }
+    
+    attack(target) {
+        console.log(`${this.name} attacks ${target.name}`);
+    }
+    
+    takeDamage(damage) {
+        this.health -= damage;
+        console.log(`${this.name} takes ${damage} damage`);
+    }
+        */
+}
