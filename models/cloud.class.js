224f80121d09c40ca7d4a8497fb9b9a1136ac75e
Joshua Brunke <seafortghost@gmail.com>
2025-04-28 23:27:01 +0200

    Set first background objects

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,8 +1,13 @@
 class Cloud extends MovableObject{
 
+    y = 20;
+    width = 500;
+    height = 300;
+
     constructor() {
         super().loadImage("./assets/img/5_background/layers/4_clouds/1.png");
-        
+        this.x = 10 + Math.random() * 500;
+
     }
 
 
--- models/world.class.js
+++ models/world.class.js
@@ -7,6 +7,17 @@ class World {
         new Chicken(),
     ];
 
+    clouds = [
+        new Cloud(),
+    ];
+
+    backgroundObjects = [
+        new BackgroundObject("./assets/img/5_background/layers/air.png", 0),
+        new BackgroundObject("./assets/img/5_background/layers/3_third_layer/2.png", 0, 220),
+        new BackgroundObject("./assets/img/5_background/layers/2_second_layer/1.png", 0),
+        new BackgroundObject("./assets/img/5_background/layers/1_first_layer/1.png", 0),
+    ];
+
     canvas;
     ctx;
 
@@ -20,15 +31,29 @@ class World {
     draw() {
         //Clears the canvas before drawing
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y,  this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y,  enemy.width, enemy.height);
-        });
+   
+        
+        //Draws the character, background objects, clouds and enemies on the canvas
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.backgroundObjects);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+
         //Draw is called in a loop to create an animation effect
         self = this;
         requestAnimationFrame(function() {
             self.draw();
             
         });
+    }
+    
+    addObjectsToMap(objects) {
+        objects.forEach(object => {
+            this.addToMap(object);
+        });
+    }
+
+    addToMap(movableObject) {
+        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y,  movableObject.width, movableObject.height);
     }
 }
