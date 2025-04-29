c9a40695e46c95dc5f0efc1db04c7f73a7b344f7
Joshua Brunke <seafortghost@gmail.com>
2025-04-29 22:04:49 +0200

    Adjusted chicken position and size

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,9 @@
 class Chicken extends MovableObject{
 
+    y = 390;
+    width = 50;
+    height = 50;
+
     constructor() {
         super().loadImage("./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
         this.x = 200 + Math.random() * 500;
