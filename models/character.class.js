6e89abcb26204bf899b4c311fee876e70abed513
Joshua Brunke <seafortghost@gmail.com>
2025-05-01 17:36:08 +0200

    Added mirroring to character

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject{
     y = 140;
     width = 100;
     height = 300;
+    speed = 10;
 
     imagesWalking = [
         "./assets/img/2_character_pepe/2_walk/W-21.png",
@@ -25,18 +26,32 @@ class Character extends MovableObject{
     /**
      * Method to animate the character
      * @description This method is called in a loop to create an animation effect.
-     * It changes the image of the character every 100ms (1000ms / 10 = 100ms per frame).
+     * It changes the image of the character.
      */
     animate() {
+
         setInterval(() => {
-            if (this.world.keyboard.RIGHT && this.x < 200) {
+            if (this.world.keyboard.RIGHT && this.x < 620) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            } else if (this.world.keyboard.LEFT && this.x > 0) {
+                this.x -= this.speed;
+                this.otherDirection = true;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT && this.x < 620 || this.world.keyboard.LEFT && this.x > 0) {
+                //Walk animation
                 let i = this.currentImage % this.imagesWalking.length; 
                 let path = this.imagesWalking[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
+            } else {
+                //Idle animation
+                this.img = this.imageCache["./assets/img/2_character_pepe/2_walk/W-21.png"];
             }
-
-        }, 1000 / 10);
+        }, 50);
     }
     /**
      * Method to make the character jump
