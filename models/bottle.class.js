80ea25a9163e976e6df402d0aea68dfb36acc8ba
Joshua Brunke <seafortghost@gmail.com>
2025-05-09 17:02:10 +0200

    Fixed bottle offset to accommodate bottles leaning left and right

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,22 +1,21 @@
 class Bottle extends MovableObject {
     constructor(x, y) {
       super();
-      const bottleImage = Math.random() < 0.5
-        ? "./assets/img/6_salsa_bottle/1_salsa_bottle_on_ground.png"
-        : "./assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png";
+      const image1 = "./assets/img/6_salsa_bottle/1_salsa_bottle_on_ground.png";
+      const image2 = "./assets/img/6_salsa_bottle/2_salsa_bottle_on_ground.png";
   
+      const bottleImage = Math.random() < 0.5 ? image1 : image2;
       this.loadImage(bottleImage);
       this.x = x;
       this.y = y;
       this.width = 60;
       this.height = 60;
   
-      this.offset = {
-        top: 5,
-        bottom: 5,
-        left: 5,
-        right: 5,
-      };
+      if (bottleImage === image1) {
+        this.offset = { top: 10, bottom: 8, left: 24, right: 14 };
+      } else {
+        this.offset = { top: 10, bottom: 8, left: 19, right: 19 };
+      }
     }
   }
   
