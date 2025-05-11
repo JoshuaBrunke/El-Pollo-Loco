5dacf9d27293d29ad29cfd9a4cac3a41ce077d92
Joshua Brunke <seafortghost@gmail.com>
2025-05-11 19:34:43 +0200

    Extended level end to the right and added another set of background images to let Pepe get closer to the endboss

--- levels/level1.js
+++ levels/level1.js
@@ -55,6 +55,11 @@ const bottlePositions = [
       new BackgroundObject("./assets/img/5_background/layers/3_third_layer/2.png", 719 * 3),
       new BackgroundObject("./assets/img/5_background/layers/2_second_layer/2.png", 719 * 3),
       new BackgroundObject("./assets/img/5_background/layers/1_first_layer/2.png", 719 * 3),
+
+      new BackgroundObject("./assets/img/5_background/layers/air.png", 719 * 4),
+      new BackgroundObject("./assets/img/5_background/layers/3_third_layer/1.png", 719 * 4),
+      new BackgroundObject("./assets/img/5_background/layers/2_second_layer/1.png", 719 * 4),
+      new BackgroundObject("./assets/img/5_background/layers/1_first_layer/1.png", 719 * 4),
   
       ...bottles, // Spread the bottle objects into the background objects array
       ...coins // Spread the coin objects into the background objects array
