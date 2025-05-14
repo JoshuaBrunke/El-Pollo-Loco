8fc9ae40c37076cc4cbcf5801e74bfab0bee4c8e
Joshua Brunke <seafortghost@gmail.com>
2025-05-14 21:31:12 +0200

    Refactored movable-object class

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -9,23 +9,24 @@ class MovableObject extends DrawableObject {
   energy = 100;
   lastHit = 0;
 
-  offset = {
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  };
+  offset = { top: 0, bottom: 0, left: 0, right: 0 };
 
   applyGravity() {
-    setInterval(() => {
-      if (this.isAboveGround() || this.speedY > 0) {
-        this.y -= this.speedY;
-        this.speedY -= this.acceleration;
-      } else if (this.y > GROUND_LEVEL - 5 && this.y <= GROUND_LEVEL + 5) {
-        this.speedY = 0;
-        this.y = GROUND_LEVEL;
-      }
-    }, 1000 / 25);
+    setInterval(() => this.applyGravityStep(), 1000 / 25);
+  }
+
+  applyGravityStep() {
+    if (this.isAboveGround() || this.speedY > 0) {
+      this.y -= this.speedY;
+      this.speedY -= this.acceleration;
+    } else if (this.isNearGround()) {
+      this.speedY = 0;
+      this.y = GROUND_LEVEL;
+    }
+  }
+
+  isNearGround() {
+    return this.y > GROUND_LEVEL - 5 && this.y <= GROUND_LEVEL + 5;
   }
 
   isAboveGround() {
@@ -34,11 +35,27 @@ class MovableObject extends DrawableObject {
 
   isColliding(mo) {
     return (
-      this.x + this.width - this.offset.right > mo.x + mo.offset.left &&
-      this.x + this.offset.left < mo.x + mo.width - mo.offset.right &&
-      this.y + this.height - this.offset.bottom > mo.y + mo.offset.top &&
-      this.y + this.offset.top < mo.y + mo.height - mo.offset.bottom
+      this.rightEdge() > mo.leftEdge() &&
+      this.leftEdge() < mo.rightEdge() &&
+      this.bottomEdge() > mo.topEdge() &&
+      this.topEdge() < mo.bottomEdge()
     );
+  }
+
+  rightEdge() {
+    return this.x + this.width - this.offset.right;
+  }
+
+  leftEdge() {
+    return this.x + this.offset.left;
+  }
+
+  bottomEdge() {
+    return this.y + this.height - this.offset.bottom;
+  }
+
+  topEdge() {
+    return this.y + this.offset.top;
   }
 
   playAnimation(images) {
@@ -61,24 +78,22 @@ class MovableObject extends DrawableObject {
   }
 
   canBeHit() {
-    const now = new Date().getTime();
+    const now = Date.now();
     return now - this.lastHit > 1000;
   }
 
-hit(damage = 5) {
-  if (!this.canBeHit()) return;
-  this.energy -= damage;
-  if (this.energy < 0) this.energy = 0;
-  this.lastHit = new Date().getTime();
-}
-
+  hit(damage = 5) {
+    if (!this.canBeHit()) return;
+    this.energy -= damage;
+    if (this.energy < 0) this.energy = 0;
+    this.lastHit = Date.now();
+  }
 
   isDead() {
     return this.energy <= 0;
   }
 
   isHurt() {
-    const timeSinceLastHit = new Date().getTime() - this.lastHit;
-    return timeSinceLastHit < 1000;
+    return Date.now() - this.lastHit < 1000;
   }
 }
