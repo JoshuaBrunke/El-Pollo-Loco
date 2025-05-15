ccd04dc598caabb38835ca6132599146370d6c79
Joshua Brunke <seafortghost@gmail.com>
2025-05-15 13:05:37 +0200

    JSDOCS for Coin class

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,5 +1,16 @@
+/**
+ * Class for the collectable coin object in the game.
+ * Coins appear in the air and can be collected by jumping.
+ * The collected coins are counted for the coin score shown if the player wins the game.
+ */
 class Coin extends MovableObject {
-    constructor(x, y) {
+  /**
+   * Creates a new Coin object at the given position.
+   * 
+   * @param {number} x - The horizontal position of the coin.
+   * @param {number} y - The vertical position of the coin.
+   */  
+  constructor(x, y) {
       super();
       this.loadImage("./assets/img/8_coin/coin_2.png");
       this.x = x;
