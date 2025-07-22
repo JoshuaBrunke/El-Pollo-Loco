d25322ad6e7c57e031a228572b1a3ecb6f1376b8
Joshua Brunke <seafortghost@gmail.com>
2025-07-22 12:19:02 +0200

    Increased coin size

--- models/coin.class.js
+++ models/coin.class.js
@@ -16,13 +16,13 @@ class Coin extends MovableObject {
       this.loadImage("./assets/img/8_coin/coin_2.png");
       this.x = x;
       this.y = y;
-      this.width = 50;
-      this.height = 50;
+      this.width = 100;
+      this.height = 100;
       this.offset = {
-        top: 12,
-        bottom: 12,
-        left: 12,
-        right: 12
+        top: 25,
+        bottom: 25,
+        left: 25,
+        right: 25
       };
     }
   }
